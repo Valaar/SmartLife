5d5408721ee9bab5f28c70e38ba30db5230c825a
windows10 <li4.sloboda@gmail.com>
2020-07-24 13:35:45 +0300

    Регистрация, логин, запрос в друзья, отправка письма, восстановление пароля.

--- src/test/java/SLCreateUser/CreateNewUser.java
+++ src/test/java/SLCreateUser/CreateNewUser.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.Assert.assertTrue;
 
 public class CreateNewUser {
 
@@ -27,6 +28,8 @@ public class CreateNewUser {
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[5]/div/button/span")).click(); //click Sign Up button
 
+        assertTrue(driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[2]/slp-todo/div/div/div[1]")).getText().contains("Getting Started"));
+
 
     }
 
@@ -47,44 +50,8 @@ public class CreateNewUser {
         driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[4]/div[2]/mat-form-field/div/div[1]/div[3]/input")).sendKeys("Qwerty123"); //enter password
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[5]/div/button/span")).click(); //click Sign Up button
-    }
 
-    @Test
-    public void CreateUser3() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/windows10/ChromeDriver/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 15);
-        driver.manage().window().maximize();
-
-        driver.get("https://smartlifepath-client-qa.herokuapp.com/web-site/home");
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/mat-tab-header/div[2]/div/div/div[2]/div")).click(); //click Sign Up button
-
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[1]/div[2]/mat-form-field/div/div[1]/div[3]/input")));
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[1]/div[2]/mat-form-field/div/div[1]/div[3]/input")).sendKeys("Test_icloud"); //enter name
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[2]/div[2]/mat-form-field/div/div[1]/div[3]/input")).sendKeys("vallaar@icloud.com"); //enter email
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[3]/div[2]/mat-form-field/div/div[1]/div[3]/input")).sendKeys("KT100NE"); //enter postcode
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[4]/div[2]/mat-form-field/div/div[1]/div[3]/input")).sendKeys("Qwerty123"); //enter password
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[5]/div/button/span")).click(); //click Sign Up button
-    }
-
-    @Test
-    public void CreateUser4() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/windows10/ChromeDriver/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 15);
-        driver.manage().window().maximize();
-
-        driver.get("https://smartlifepath-client-qa.herokuapp.com/web-site/home");
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/mat-tab-header/div[2]/div/div/div[2]/div")).click(); //click Sign Up button
-
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[1]/div[2]/mat-form-field/div/div[1]/div[3]/input")));
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[1]/div[2]/mat-form-field/div/div[1]/div[3]/input")).sendKeys("Test_aol"); //enter name
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[2]/div[2]/mat-form-field/div/div[1]/div[3]/input")).sendKeys("test_121212@aol.com"); //enter email
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[3]/div[2]/mat-form-field/div/div[1]/div[3]/input")).sendKeys("KT100NE"); //enter postcode
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[4]/div[2]/mat-form-field/div/div[1]/div[3]/input")).sendKeys("Qwerty123"); //enter password
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[2]/div/form/div[5]/div/button/span")).click(); //click Sign Up button
+        assertTrue(driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[2]/slp-todo/div/div/div[1]")).getText().contains("Getting Started"));
 
         driver.quit();
     }
