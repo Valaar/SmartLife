7c399536a32554cecd8ad53012d35b0e1638be16
windows10 <li4.sloboda@gmail.com>
2020-08-25 16:56:21 +0300

    update

--- src/test/java/SLCreateUser/TestFriendRequestAndMessage.java
+++ src/test/java/SLCreateUser/TestFriendRequestAndMessage.java
@@ -21,7 +21,7 @@ public class TestFriendRequestAndMessage {
 
         System.setProperty("webdriver.chrome.driver", "C:/Users/windows10/ChromeDriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 15);
+        WebDriverWait wait = new WebDriverWait(driver, 25);
         driver.manage().window().maximize();
 
         driver.get("https://smartlifepath-client-qa.herokuapp.com/web-site/home");
@@ -30,10 +30,13 @@ public class TestFriendRequestAndMessage {
         driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[1]/div/form/div[2]/div/mat-form-field/div/div[1]/div[3]/input")).sendKeys("Qwerty123"); //enter password
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[1]/div/form/div[3]/div/button/span")).click(); //click LogIn button
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[2]/slp-todo/div/div/div[1]")));
-        assertTrue(driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[2]/slp-todo/div/div/div[1]")).getText().contains("Getting Started"));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[1]/div[1]/slp-my-contributions-stats/div/div[1]/h1")));
+        assertTrue(driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[1]/div[1]/slp-my-contributions-stats/div/div[1]/h1")).getText().contains("My contributions"));
 
-        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-header/div/div[2]/button[1]/span/mat-icon[2]")).click(); //open Find Menu
+        // Find a person and send a friend request
+
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/slp-stream-header/div/div[1]/p[2]")));
+        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-header/div/div[2]/button[1]/span/mat-icon[1]")).click(); //open Find Menu
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div/button[6]")).click(); //find friends
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/div/div[2]/div/mat-dialog-container/slp-find-friends/slp-dialog-wrapper/div[2]/div[1]/mat-form-field/div/div[1]/div[4]/input")));
@@ -43,9 +46,9 @@ public class TestFriendRequestAndMessage {
         Thread.sleep(500);
         driver.findElement(By.xpath("/html/body/div/div[2]/div/mat-dialog-container/slp-find-friends/slp-dialog-wrapper/div[2]/button/span")).click(); //click Send button
         Thread.sleep(500);
-        assertTrue(driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[2]/slp-todo/div/div/div[1]")).getText().contains("Getting Started"));
+        assertTrue(driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[1]/div[1]/slp-my-contributions-stats/div/div[1]/h1")).getText().contains("My contributions"));
 
-
+        driver.quit();
     }
 
     @Test
@@ -63,20 +66,22 @@ public class TestFriendRequestAndMessage {
         driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[1]/div/form/div[2]/div/mat-form-field/div/div[1]/div[3]/input")).sendKeys("Qwerty123"); //enter password
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[1]/div/form/div[3]/div/button/span")).click(); //click LogIn button
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[2]/slp-todo/div/div/div[1]")));
-        assertTrue(driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[2]/slp-todo/div/div/div[1]")).getText().contains("Getting Started"));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[1]/div[1]/slp-my-contributions-stats/div/div[1]/h1")));
+        assertTrue(driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[1]/div[1]/slp-my-contributions-stats/div/div[1]/h1")).getText().contains("My contributions"));
 
         driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-header/div/div[2]/a[2]/span/mat-icon")).click(); //open friends list
         Thread.sleep(500);
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div/slp-friend-list/div[1]/div/div/span[1]")).click(); //accept request
         Thread.sleep(500);
+        /*
         driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-header/div/div[2]/a[2]/span/mat-icon")).click(); //open friends list
         Thread.sleep(500);
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div/slp-friend-list/div[1]/div/div/div[2]/div[2]/span")).click(); //click to Direct message
-        Thread.sleep(500);
-        driver.findElement(By.xpath("/html/body/div/div[2]/div/mat-dialog-container/slp-send-message-dialog/slp-dialog-wrapper/div[2]/div/div[3]/div/textarea")).sendKeys("Test message"); //message
+        Thread.sleep(1500);
+        //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/div[3]/div[2]/div/mat-dialog-container/slp-send-message-dialog/slp-dialog-wrapper/div[3]/div/div/slp-select-file/input")));
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/mat-dialog-container/slp-send-message-dialog/slp-dialog-wrapper/div[2]/div/div[6]/div/textarea")).sendKeys("Test message"); //message
         driver.findElement(By.xpath("/html/body/div/div[2]/div/mat-dialog-container/slp-send-message-dialog/slp-dialog-wrapper/div[3]/div/button/span")).click(); //click Send button
-
+*/
         driver.quit();
 
 
---
+++ src/test/java/TestRemovingFromFriendsList.java
@@ -0,0 +1,44 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestRemovingFromFriendsList {
+
+    @Test
+    public void TestDeleteProfile() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/windows10/ChromeDriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        driver.manage().window().maximize();
+
+        driver.get("https://smartlifepath-client-qa.herokuapp.com/web-site/home");
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[1]/div/form/div[1]/div/mat-form-field/div/div[1]/div[3]/input")));
+        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[1]/div/form/div[1]/div/mat-form-field/div/div[1]/div[3]/input")).sendKeys("tester09876543210@yahoo.com"); //enter login
+        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[1]/div/form/div[2]/div/mat-form-field/div/div[1]/div[3]/input")).sendKeys("Qwerty123"); //enter password
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-website/section/app-home/div/div/section[1]/div/div[2]/div[2]/div/mat-tab-group/div/mat-tab-body[1]/div/form/div[3]/div/button/span")).click(); //click LogIn button
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[1]/div[1]/slp-my-contributions-stats/div/div[1]/h1")));
+        assertTrue(driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-my-stream/div/div[1]/div[1]/slp-my-contributions-stats/div/div[1]/h1")).getText().contains("My contributions"));
+
+        //Open friend list and delete friend
+
+        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-header/div/div[2]/a[2]/span/mat-icon")).click(); //open friends list
+        //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"mat-menu-panel-1\"]/div/slp-friend-list/div[1]/div/div/div[2]/div[1]")));
+        driver.findElement(By.xpath("//*[@id=\"mat-menu-panel-1\"]/div/slp-friend-list/div[1]/div/div/div[2]/div[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/app-root/section/app-slp-platform/section/app-slp-header/div/div[2]/a[2]/span/mat-icon")).click();
+        Thread.sleep(1500);
+
+        Assert.assertTrue(driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/slp-friend-list/div/button")).getText().contains(" Find and invite friends to smart life path "));
+
+        driver.quit();
+
+
+    }
+}
